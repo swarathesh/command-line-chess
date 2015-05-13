a41d1c1f3659a0c9229478fcff05e6c5d8250d04
omercelik <omercelik@139.179.92.25>
2015-05-13 22:22:52 +0300

    com

--- MadChess/src/Test.java
+++ MadChess/src/Test.java
@@ -5,12 +5,41 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-	//	Board b = new Board();
-
-		//System.out.println("\u2709\uFE0E");
-		GameEngine gameEngine = new GameEngine();
-
-		gameEngine.attemptMove();
+		
+		Scanner scan = new Scanner(System.in);
+		String s = "Welcome to Mad Chess"
+				+ "\n1. play game"
+				+ "\n2. see instructions"
+				+ "\n3.see credits"
+				+ "\n4 Quit.\n";
+		
+		System.out.println(s);
+		System.out.println("enter a number");
+		int input = scan.nextInt();
+		
+		
+		while(input >1 || input < 4){
+			
+			if(input == 1){
+				GameEngine gameEngine = new GameEngine();
+				gameEngine.attemptMove();
+			}
+			else if(input ==2){
+				
+			}
+			else if(input == 3){
+				
+			}
+			else{
+				
+			}
+			
+			System.out.println("enter correct number");
+			input = scan.nextInt();
+			
+		}
+		
+		
 	
 		
 		
